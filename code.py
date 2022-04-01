baea8602f81121f7ef2ebe794dd9bd3cadd8b233
Ethan-Prieur1 <99287118+Ethan-Prieur1@users.noreply.github.com>
2022-04-01 14:14:53 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Created by: Ethan Prieur
-# Created on: March 2022
+# Created on: April 2022
 # This program is the "Space Aliens" program on the PyBadge
 
 import stage
@@ -10,9 +10,22 @@ import ugame
 
 def game_scene():
     # this function is the main game game_scene
-
-    print("\n\n\n")  # 3 blank lines
-    print("Hello World!")
+    
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    
+    # set the background to image 9 in the image Bank
+    #   and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+    
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render all sprites
+    #   most likely you will only render the background once per game game_scene
+    game.render_block()
 
     # repeat forever, game loop
     while True:
