77adeb2e7a8f355923b2f1488954a6a44d8fc9bc
Ethan-Prieur1 <99287118+Ethan-Prieur1@users.noreply.github.com>
2022-04-22 14:39:55 -0400

    Update code.py

--- code.py
+++ code.py
@@ -10,27 +10,28 @@ import stage
 import time
 import ugame
 
+
 def splash_scene():
     # this function is the splash scene
-    
+
     # get sound ready
-    coin_sound = open("coin.wav", 'rb')
+    coin_sound = open("coin.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
     sound.play(coin_sound)
-    
+
     # an image bank for circuit python
     image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
-    
+
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     # sets the background to image 0 in the image bank
-    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
-                            constants.SCREEN_Y)
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,constants.SCREEN_Y
+    )
     # used this program to split the image into tile:
     #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
-    background.tile(2, 2, 0) # blank white
+    background.tile(2, 2, 0)  # blank white
     background.tile(3, 2, 1)
     background.tile(4, 2, 2)
     background.tile(5, 2, 3)
@@ -56,9 +57,9 @@ def splash_scene():
     background.tile(4, 5, 13)
     background.tile(5, 5, 14)
     background.tile(6, 5, 0)
-    background.tile(7, 5, 0) # blank white
-    
-    
+    background.tile(7, 5, 0)  # blank white
+
+
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
@@ -67,35 +68,38 @@ def splash_scene():
     # render the background and initial location of sprite list
     # most likely you will only render background once per scene
     game.render_block()
-    
+
     # repeat forever, game loop
     while True:
         # Wait for 2 seconds
         time.sleep(2.0)
         menu_scene()
-        
+
+
 def menu_scene():
     # this function is the main game game_scene
 
     # image banks for CircuitPython
     image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
-   # add text objects
+    # add text objects
     text = []
-    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
+    text1 = stage.Text(
+        width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None
+    )
     text1.move(20, 10)
     text1.text("MT Game Studios")
     text.append(text1)
-   
+
     text2 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
     text2.move(40, 110)
     text2.text("PRESS START")
     text.append(text2)
-   
+
     # sets the background to image 0 in the image bank
-    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
-                            constants.SCREEN_Y)
-    
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,constants.SCREEN_Y
+    )
+
     # create a stage for the background to show up on
     #    and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
@@ -104,23 +108,21 @@ def menu_scene():
     # render the backtground and initial location of sprite list
     # most likekly you will only render background once per game_scene
     game.render_block()
-   
+
     # repeat forever, game loop
     while True:
         # get user input
         keys = ugame.buttons.get_pressed()
-       
+
         # Start button selected
         if keys & ugame.K_START != 0:
             game_scene()
-           
+
             # update game logic
             game.tick() # wait until refresh rate finished
-           
-
 
 def game_scene():
-# this function is the main game game_scene
+    # this function is the main game game_scene
 
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
@@ -131,17 +133,18 @@ def game_scene():
     b_button = constants.button_state["button_up"]
     start_button = constants.button_state["button_up"]
     select_button = constants.button_state["button_up"]
-    
+
     # get sound ready
-    pew_sound = open("pew.wav", 'rb')
+    pew_sound = open("pew.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
 
     # set the background to image 0 in the image Bank
     #   and the size (10x8 tiles of size 16x16)
-    background = stage.Grid(image_bank_background, constants.SCREEN_X,
-                            constants.SCREEN_Y)
+    background = stage.Grid(
+        image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
+    )
     for x_location in range(constants.SCREEN_GRID_X):
         for y_location in range(constants.SCREEN_GRID_Y):
             tile_picked = random.randint(1, 3)
@@ -151,9 +154,12 @@ def game_scene():
         image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
     )
 
-    alien = stage.Sprite(image_bank_sprites, 9,
+    alien = stage.Sprite(
+        image_bank_sprites, 
+        9,
         int(constants.SCREEN_X / 2 - constants.SPRITE_SIZE / 2),
-        16)
+        16,
+    )
 
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
@@ -170,7 +176,7 @@ def game_scene():
         keys = ugame.buttons.get_pressed()
 
         # A button to fire
-        if keys & ugame.K_O !=0:
+        if keys & ugame.K_O != 0:
             if a_button == constants.button_state["button_up"]:
                 a_button = constants.button_state["button_just_pressed"]
             elif a_button == constants.button_state["button_just_pressed"]:
@@ -190,7 +196,7 @@ def game_scene():
         if keys & ugame.K_SELECT != 0:
             pass
 
-      
+
         if keys & ugame.K_RIGHT != 0:
             if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
                 ship.move(ship.x + 1, ship.y)
